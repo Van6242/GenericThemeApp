0a8ecbfa42d50f765a3d934cde41fdd556fd42fa
Van6242 <qhart52336@gmail.com>
2022-05-31 09:46:33 -0400

    list app

--- GenericThemeApp/ViewController.swift
+++ GenericThemeApp/ViewController.swift
@@ -7,13 +7,78 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    var myList = ["hi"]
 
-    override func viewDidLoad() {
+    @IBOutlet weak var myTextf: UITextField!
+    @IBOutlet weak var myTableview: UITableView!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myTableview.delegate = self
+        myTableview.dataSource = self
+        loadFromUserDefalts()
+    }
+    @IBAction func addButton(_ sender: Any)
+    {
+        //get name
+        let name = myTextf.text!
+        myList.append(name)
+        myTableview.reloadData()
+        
+        myTextf.text = ""
+        saveToUserDefalts()
+        
+        
+    }
+    var canedit = false
+    @IBAction func delbtn(_ sender: Any)
+    {
+        canedit = !canedit
+        myTableview.setEditing(canedit, animated: true)
+        myTableview.reloadData()
     }
 
 
+    func saveToUserDefalts()
+    {
+        let defalts = UserDefaults.standard
+        defalts.setValue(myList, forKey: "mylist")
+    }
+    func loadFromUserDefalts()
+    {
+        let defalts = UserDefaults.standard
+        myList = defalts.object(forKey: "mylist") as! [String]
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == .delete
+        {
+            myList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .top)
+            saveToUserDefalts()
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return myList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+       
+        let item = myList[indexPath.row]
+        cell.textLabel?.text = item
+        //set other things here too
+        return cell
+    
+    }
+ 
+    
 }
 
